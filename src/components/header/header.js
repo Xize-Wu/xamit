ed919040693d55714afbf4cebdb20d2ad263be89
Eregion <wuxize1995@gmail.com>
2023-10-14 22:23:26 -0400

    add single test card

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,15 @@
+import Link from 'next/link'
+
 export default function Header() {
     return (<>
-        <div class="bg-indigo-500 p-6">
-            <div class="container mx-auto">
-                <h1 class="text-white text-3xl font-bold">Xamit!</h1>
+        <div className="bg-indigo-500 p-6 shadow-lg flex">
+            <div className="container mx-auto">
+                <Link href="/"><h1 className="text-white text-3xl font-bold">Xamit!</h1></Link>
+            </div>
+            <div>
+            <span className="mx-auto font-semibold text-white px-3">Hello! place_holder@email.com</span>
+            <button class="bg-pink-500 text-white font-semibold px-4 py-2 rounded-md mt-4 hover:bg-pink-600">Log out</button>
+
             </div>
         </div>
     </>)
