3402a0ad91a4b4f967794cbc1860dd505728c722
Eregion <wuxize1995@gmail.com>
2023-10-24 21:01:30 -0400

    change button color

--- src/components/frontpage/Info.js
+++ src/components/frontpage/Info.js
@@ -6,7 +6,7 @@ export default function MyClasses() {
       <div className="container mx-auto p-6">
         <div className="flex justify-between mb-4">
           <h1 className="text-2xl font-semibold mt-4  text-gray-600">My Classes</h1>
-          <button class="bg-indigo-500 text-white font-semibold px-4 py-2 rounded-md mt-4 hover:bg-indigo-600">Add New Class</button>
+          <button class="bg-indigo-500 text-white font-semibold px-4 py-2 rounded-md mt-4 hover:bg-pink-500">Add New Class</button>
         </div>
         <div className="flex-row my-10">
           <div className="m-4">
